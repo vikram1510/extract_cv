0743639f7f636c25f924dac83b7e7eb319ad9837
Vikram Bageja <vikram.bageja@gmail.com>
2021-03-04 16:19:36 +0000

    Add frontend

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
 
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send('ye');
+app.use(cors());
+
+app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'k' });
 });
 
-app.listen(4000, () => console.log('listening'));
+app.post('/upload', (req , res) => {
+  console.log(req.files!.review);
+
+  res.json({ message: 'j' });
+});
+
+app.listen(4000, '0.0.0.0',  () => console.log('listening'));
 
